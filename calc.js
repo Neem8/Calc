f076901ddbd4abcd1436060e8058c5ea5d8584cf
Neem8 <87055612+Neem8@users.noreply.github.com>
2022-05-28 10:57:07 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById('output-Screen')
+
+function Display(num){
+    outputScreen.value+=num;
+}
+
+function Calculate(){
+    try{
+        outputScreen.value=eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid");
+    }
+}
+
+function Clear(){
+    outputScreen.value="";
+}
+
+function Delete(){
+    outputScreen.value=outputScreen.value.slice(0,-1);
+}
